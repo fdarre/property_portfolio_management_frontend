d8c05e2599b6bbb6a79270bf0983bfd170c3b155
Frédéric Darré <frederic.darre@outlook.com>
2020-06-12 11:26:32 +0200

    Add BuildingCard component
    The function based App component is converted to a class based App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import BuildingCard from './components/BuildingCard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return(
+        <div className="App">
+          <BuildingCard />
+        </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/BuildingCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BuildingCard extends React.Component {
+    render() {
+        return <h1>Building name</h1>;
+    }
+}
+
+export default BuildingCard;
